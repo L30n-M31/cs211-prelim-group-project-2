8739269968938eeba61a28002cff1c6b766faa1b
Leonhard Leung <leonhardleung@gmail.com>
2023-10-04 21:44:05 +0800

    finished the Export class

--- src/util/Export.java
+++ src/util/Export.java
@@ -1,4 +1,25 @@
+/**
+ * @author: Leung, Leonhard
+ * Date: 10/04/2023
+ */
+
 package util;
 
+import list.LinkedList;
+import java.io.*;
+
 public class Export {
-}
+    public void exportTo(LinkedList<Object> list, String filename) throws IOException {
+        String defaultDirectory = "csv_exports/";
+
+        File file = new File(defaultDirectory + filename + ".csv");
+
+        PrintWriter printWriter = new PrintWriter(file);
+
+        for (int i = 0; i < list.size(); i++) {
+            printWriter.println(list.get(i).toString());
+        }
+
+        printWriter.close();
+    } // end of exportTo method
+} // end of Exporter class
